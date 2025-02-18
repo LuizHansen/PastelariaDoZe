f0ae4d066f8cb90f6418c7e17b1de49c0ac78581
Luiz_Hansen <luizhansen751@gmail.com>
2025-02-18 16:59:49 -0300

    Adicionando API

---
+++ scr/domain/entities/Cliente.py
@@ -0,0 +1,8 @@
+#Luiz Henrique Hansen
+from pydantic import BaseModel
+
+class Cliente(BaseModel):
+    id_cliente: int = None
+    nome: str
+    cpf: str
+    telefone: str
---
+++ scr/domain/entities/Produto.py
@@ -0,0 +1,9 @@
+#Luiz Henrique Hansen
+from pydantic import BaseModel
+
+class Produto(BaseModel):
+    id_produto: int = None
+    nome: str
+    descricao: str = None
+    foto: None
+    valor_unitario: int
